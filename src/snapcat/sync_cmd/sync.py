40615313dc7075b5d4071309e0e9ba9dec310fd0
Yak <y@kuhi.to>
2025-10-05 17:41:07 +0800

    update a tiny bit of sync code

--- src/snapcat/sync_cmd/sync.py
+++ src/snapcat/sync_cmd/sync.py
@@ -4,7 +4,8 @@ from chia_rs.sized_bytes import bytes32
 from chia_rs.sized_ints import uint32
 from clvm.casts import int_to_bytes
 from chia.util.hash import std_hash
-from chia.wallet.cat_wallet.cat_utils import CAT_MOD
+from chia.wallet.cat_wallet.cat_utils import CAT_MOD, CAT_MOD_HASH
+from chia.wallet.vc_wallet.vc_drivers import REVOCATION_LAYER, REVOCATION_LAYER_HASH
 import logging
 from typing import List, Optional, Tuple
 
@@ -28,6 +29,7 @@ async def get_full_node_synced(
 async def process_coin_spends(
     db,
     expected_tail_hash: bytes32,
+    hidden_puzzle_hash: bytes32 | None = None,
     height,
     header_hash: str,
     coin_spends: Optional[List[CoinSpend]],
@@ -43,7 +45,7 @@ async def process_coin_spends(
     )
 
     for coin_spend in coin_spends:
-        result = extract_cat(expected_tail_hash, coin_spend)
+        result = extract_cat(expected_tail_hash, hidden_puzzle_hash, coin_spend)
 
         if result is None:
             log.debug(f"{expected_tail_hash.hex()} CAT coin spend not found")
@@ -70,11 +72,19 @@ async def process_coin_spends(
                 ],
             )
             for coin in inner_puzzle_create_coin_conditions:
+                inner_puzzle_hash = coin.puzzle_hash
+                if hidden_puzzle_hash is not None:
+                    inner_puzzle_hash = REVOCATION_LAYER.curry(
+                        REVOCATION_LAYER_HASH,
+                        hidden_puzzle_hash,
+                        inner_puzzle_hash,
+                    ).get_tree_hash()
+
                 outer_puzzle_hash = CAT_MOD.curry(
-                    CAT_MOD.get_tree_hash(),
+                    CAT_MOD_HASH,
                     expected_tail_hash,
-                    coin.puzzle_hash,
-                ).get_tree_hash_precalc(coin.puzzle_hash)
+                    inner_puzzle_hash,
+                ).get_tree_hash_precalc(inner_puzzle_hash)
 
                 created_coin_name = std_hash(
                     bytes32.fromhex(coin_spend_coin_name)
@@ -96,7 +106,11 @@ async def process_coin_spends(
 
 
 async def process_block(
-    full_node_rpc: FullNodeRpcClient, db, tail_hash: bytes32, height: int
+    full_node_rpc: FullNodeRpcClient,
+    db,
+    tail_hash: bytes32,
+    hidden_puzzle_hash: bytes32 | None = None,
+    height: int,
 ):
     block_record = await full_node_rpc.get_block_record_by_height(height)
     if block_record is None:
@@ -113,7 +127,12 @@ async def process_block(
         if coin_spends is not None and len(coin_spends) > 0:
             log.debug("%i spends found in block %i", len(coin_spends), height)
             await process_coin_spends(
-                db, tail_hash, height, block_record.header_hash, coin_spends
+                db,
+                tail_hash,
+                hidden_puzzle_hash,
+                height,
+                block_record.header_hash,
+                coin_spends,
             )
 
         else:
